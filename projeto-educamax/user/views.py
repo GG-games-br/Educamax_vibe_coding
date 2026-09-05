5005114a9aaff26d152163dc95cf36156be54824
GG-games-br <laptopfanyeah@gmail.com>
2026-09-05 14:12:03 -0300

    Html on main

--- projeto-educamax/user/views.py
+++ projeto-educamax/user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def index_view(request):
-    return render(request, 'main/home.html')
+def index(request):
+    return HttpResponse('mabbals')
