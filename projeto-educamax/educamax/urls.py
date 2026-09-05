eb679633672dc7dc276dce3da0d1917359231d0b
GG-games-br <laptopfanyeah@gmail.com>
2026-09-05 14:18:30 -0300

    Adding htmls to templates

--- projeto-educamax/educamax/urls.py
+++ projeto-educamax/educamax/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
+Caminhos para adicionar
+
+path('user/'include('user.urls'))
+"""
+
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
